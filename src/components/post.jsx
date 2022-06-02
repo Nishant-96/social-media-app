2c8da8f5c40b9ab1d6f139490179e8bbd12ba7db
Nishant-96 <nishant11pratihast@gmail.com>
2022-06-02 21:29:29 +0530

    chore: home and post component

---
+++ src/components/post.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import CommentIcon from "@mui/icons-material/Comment";
+import BookmarkBorderOutlinedIcon from "@mui/icons-material/BookmarkBorderOutlined";
+
+export function Post() {
+  return (
+    <div className="flex gap-4 shadow-[0px_0px_2px_#78909c] p-4">
+      <div className=" min-w-[48px]">
+        <img
+          className="h-12 w-12 rounded-full object-cover"
+          src="/assets/demo.jpg"
+          alt="avatar"
+        />
+      </div>
+      <div className="flex flex-col grow gap-3">
+        <div className="flex justify-between">
+          <div className="flex gap-2">
+            <p className="font-semibold">Chris Gayle</p>
+            <p className="text-slate-500">@chris11</p>
+          </div>
+          <MoreVertIcon className="cursor-pointer" />
+        </div>
+        <p className="text-sm tracking-wide text-justify pr-4">
+          Lorem ipsum dolor sit amet. Et porro sequi in magnam galisum aut
+          doloribus enim sit reprehenderit consequatur et atque atque eum natus
+          eius. Qui repellat dignissimos qui quasi excepturi nam commodi
+          perferendis et illo dolores sit explicabo quidem.
+        </p>
+        <div className="flex justify-between">
+          <FavoriteBorderIcon className="cursor-pointer text-slate-500" />
+          <CommentIcon className="cursor-pointer text-slate-500" />
+          <BookmarkBorderOutlinedIcon className="text-slate-500 cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Home } from "./home.jsx";
+export { Explore } from "./explore.jsx";
+export { Profile } from "./profile.jsx";
+export { Bookmarks } from "./bookmarks.jsx";
