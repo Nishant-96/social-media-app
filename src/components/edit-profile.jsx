8494389a2f50bbadec7817f9f2e0730004cfe960
Nishant-96 <nishant11pratihast@gmail.com>
2022-06-09 02:38:09 +0530

    feat: edit profile

---
+++ src/components/edit-profile.jsx
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+
+import { useDispatch, useSelector } from "react-redux";
+import { editUserHandler } from "../store/features/users-slice";
+
+import CloseIcon from "@mui/icons-material/Close";
+import CameraAltIcon from "@mui/icons-material/CameraAlt";
+export function EditProfile({ show, setShow }) {
+  const { users } = useSelector((state) => state.users);
+  const { user, token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const loggedInUser = users.filter(
+    (curr) => curr.username === user.username
+  )[0];
+
+  const [profileDetail, setProfileDetails] = useState(loggedInUser);
+
+  useEffect(() => {
+    setProfileDetails(loggedInUser);
+  }, [users, loggedInUser]);
+
+  const uploadImageHandler = async (img) => {
+    const data = new FormData();
+    data.append("file", img);
+    data.append("upload_preset", "xvk1bbmw");
+    const requestOptions = {
+      method: "POST",
+      body: data,
+    };
+    await fetch(
+      "https://api.cloudinary.com/v1_1/dnkpd7qq8/image/upload",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setProfileDetails((prev) => ({
+          ...prev,
+          avatarURL: json.url,
+        }));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  return show.edit ? (
+    <div
+      className="fixed inset-0 flex justify-center items-center z-20 bg-[#00000070] p-2"
+      onClick={() => setShow((prev) => ({ ...prev, edit: false }))}
+    >
+      <div
+        className="bg-white flex flex-col  rounded-md p-4 gap-4 max-w-md w-full"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <div className="flex justify-between">
+          <div className="text-2xl font-semibold">Edit Profile</div>
+          <CloseIcon
+            onClick={() => setShow((prev) => ({ ...prev, edit: false }))}
+          />
+        </div>
+        <div className="flex justify-center relative">
+          <img
+            className="h-20 w-20 rounded-full object-cover"
+            src={loggedInUser?.avatarURL || `/assets/demo.png`}
+            alt="avatar"
+          />
+          <label className="absolute bottom-0 right-[38%]">
+            <CameraAltIcon className="cursor-pointer text-sky-600" />
+            <input
+              type="file"
+              accept="image/apng, image/avif, image/gif, image/jpeg, image/png, image/svg+xml, image/jpg,image/webp"
+              className="hidden"
+              onChange={(event) => uploadImageHandler(event.target.files[0])}
+            />
+          </label>
+        </div>
+        <div className="flex gap-2">
+          <div>Name:</div>
+          <div>{`${loggedInUser?.firstName} ${loggedInUser?.lastName}`}</div>
+        </div>
+        <div>
+          <div>Bio:</div>
+          <input
+            className="border-2 p-2 border-solid border-slate-400 outline-sky-600 rounded h-10 w-full"
+            type="text"
+            name="bio"
+            value={profileDetail?.bio}
+            onChange={(event) =>
+              setProfileDetails((prev) => ({
+                ...prev,
+                bio: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div>
+          <div>Website:</div>
+          <input
+            className="border-2 p-2 border-solid border-slate-400 outline-sky-600 rounded h-10 w-full"
+            type="text"
+            name="website"
+            value={profileDetail?.portfolio}
+            onChange={(event) =>
+              setProfileDetails((prev) => ({
+                ...prev,
+                portfolio: event.target.value,
+              }))
+            }
+          />
+        </div>
+        <div
+          className="sidebar-btn mx-auto py-2"
+          onClick={() => {
+            token && dispatch(editUserHandler({ profileDetail, token }));
+            setShow((prev) => ({ ...prev, edit: false }));
+          }}
+        >
+          Save button
+        </div>
+      </div>
+    </div>
+  ) : null;
+}
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Post } from "../components";
+import { EditProfile, FollowListModal, Post } from "../components";
 
 import { logoutHandler } from "../store/features/auth-slice";
 
 export function Profile() {
   const dispatch = useDispatch();
   const { user } = useSelector((state) => state.auth);
+  const { users } = useSelector((state) => state.users);
+  const [showModal, setShowModal] = useState({
+    edit: false,
+    followers: false,
+    following: false,
+  });
+
+  const loggedInUser = users.filter(
+    (curr) => curr.username === user.username
+  )[0];
+
   return (
     <div className="w-[600px] p-4">
       <div className="flex flex-col shadow-[0px_0px_2px_#78909c] gap-4">
@@ -14,11 +25,16 @@ export function Profile() {
           <div className="flex justify-between items-center">
             <img
               className="h-24 w-24 rounded-full object-cover"
-              src="/assets/demo.jpg"
+              src={loggedInUser?.avatarURL || `/assets/demo.png`}
               alt="avatar"
             />
             <div className="flex gap-4 flex-wrap">
-              <div className=" font-semibold px-4 py-2 bg-transparent text-black flex justify-center cursor-pointer rounded-full border border-solid border-slate-400 transition-all  duration-300 hover:bg-slate-200">
+              <div
+                className=" font-semibold px-4 py-2 bg-transparent text-black flex justify-center cursor-pointer rounded-full border border-solid border-slate-400 transition-all  duration-300 hover:bg-slate-200"
+                onClick={() =>
+                  setShowModal((prev) => ({ ...prev, edit: true }))
+                }
+              >
                 Edit Profile
               </div>
               <div
@@ -36,18 +52,33 @@ export function Profile() {
                 user.username.split("@")[0]
               }`}</p>
             </div>
-            <div>{user.bio}</div>
+            <div>{loggedInUser?.bio}</div>
+            <div>{loggedInUser?.portfolio}</div>
             <div className="flex gap-4">
               <div className="flex gap-1">
                 <p className="font-semibold">10</p>
                 <p className="text-slate-500">Posts</p>
               </div>
-              <div className="flex gap-1">
-                <p className="font-semibold">5</p>
+              <div
+                className="flex gap-1 cursor-pointer"
+                onClick={() =>
+                  setShowModal((prev) => ({ ...prev, following: true }))
+                }
+              >
+                <p className="font-semibold">
+                  {loggedInUser?.following?.length}
+                </p>
                 <p className="text-slate-500">Following</p>
               </div>
-              <div className="flex gap-1">
-                <p className="font-semibold">15</p>
+              <div
+                className="flex gap-1 cursor-pointer"
+                onClick={() =>
+                  setShowModal((prev) => ({ ...prev, followers: true }))
+                }
+              >
+                <p className="font-semibold">
+                  {loggedInUser?.followers?.length}
+                </p>
                 <p className="text-slate-500">Followers</p>
               </div>
             </div>
@@ -62,6 +93,12 @@ export function Profile() {
           <Post />
         </div>
       </div>
+      <FollowListModal
+        show={showModal}
+        setShow={setShowModal}
+        userProfile={loggedInUser}
+      />
+      <EditProfile show={showModal} setShow={setShowModal} />
     </div>
   );
 }
