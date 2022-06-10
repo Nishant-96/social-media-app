d17e0d307e1345852b4c0ee3bb590fd01c9ef6b4
Nishant-96 <nishant11pratihast@gmail.com>
2022-06-10 09:47:31 +0530

    feat: post on single user, explore, home, bookmark

--- src/pages/bookmarks.jsx
+++ src/pages/bookmarks.jsx
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Post } from "../components";
+import { getBookmarkPostsHandler } from "../store/features/post-slice";
 
 export function Bookmarks() {
+  const { bookmarkPosts } = useSelector((state) => state.posts);
+  const bookmarkPostsArr = [...bookmarkPosts].reverse();
   return (
     <div className="w-[600px] flex flex-col p-4 ">
-      <Post />
-      <Post />
+      {bookmarkPosts.length > 0 ? (
+        bookmarkPostsArr.map((currPost) => (
+          <Post key={currPost._id} postDetail={currPost} />
+        ))
+      ) : (
+        <div className="flex gap-4 shadow-[0px_0px_2px_#78909c] p-4 items-center justify-center h-[100%]">
+          <div className="text-xl font-semibold">Save Posts for Later</div>
+        </div>
+      )}
     </div>
   );
 }
